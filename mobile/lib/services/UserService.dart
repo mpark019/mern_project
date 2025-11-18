7578f2014e3e4f024dfbfe5c7de0625b13a96943
Christian Pham <christianp@Christians-MacBook-Air.local>
2025-11-17 23:44:41 -0500

    updated the profile

--- mobile/lib/services/UserService.dart
+++ mobile/lib/services/UserService.dart
@@ -3,18 +3,33 @@ import 'package:http/http.dart' as http;
 import '../utils/GlobalData.dart';
 
 class UserService {
-  static const String baseUrl = "http://10.0.2.2:5050";
+  static const String baseUrl = "http://10.0.2.2:5050/api/users";
 
-  static Future<void> updateCalorieGoal(int goal) async {
-    final url = Uri.parse("$baseUrl/users/goal");
+  static Map<String, String> _headers() {
+    return {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer ${GlobalData.token}",
+    };
+  }
 
-    await http.patch(
-      url,
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer ${GlobalData.token}",
-      },
-      body: jsonEncode({"goal": goal}),
-    );
+  // Update calorie goal or username/email
+  static Future<Map<String, dynamic>> updateUser(String id, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final res = await http.patch(url, headers: _headers(), body: jsonEncode(body));
+    return jsonDecode(res.body);
+  }
+
+  // Change password
+  static Future<Map<String, dynamic>> changePassword(String id, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl/$id/password");
+    final res = await http.patch(url, headers: _headers(), body: jsonEncode(body));
+    return jsonDecode(res.body);
+  }
+
+  // Fetch current user profile
+  static Future<Map<String, dynamic>> getMe() async {
+    final url = Uri.parse("$baseUrl/me");
+    final res = await http.get(url, headers: _headers());
+    return jsonDecode(res.body);
   }
 }
